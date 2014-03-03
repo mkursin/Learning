65ab919d4b90861ca53fc0ad817db4b9fca405af
mkursin <kursik88@gmail.com>
2014-03-03 10:59:28 +0400

    Комментарии для кода, рефакторинг классов фигур

--- php/geometry_figures.php
+++ php/geometry_figures.php
@@ -5,49 +5,109 @@
  */
 
 /*
- * Построим систему классов для описания плоских геометрических фигур: круга, квадрата, прямоугольника. Предусмотрим методы для изменения размеров.
+ * Система классов для описания плоских геометрических фигур: круга, квадрата, прямоугольника. Предусмотрим методы для изменения размеров.
  */
 
-   class Geometry_Circle
-    {
+/**
+ * Class Circle
+ * Класс описывающий окружность
+ */
+class Circle
+{
+	/**
+	 * Радиус окружности
+	 * @var
+	 */
+	var $radius;
 
-        function circle()
-        {   $radius = 10;
-	        $area =3.14 * $radius *  $radius;
-	       return $area;
-        }
-    }
-
-	$result = new Geometry_Circle();
-	echo $result ->circle();
-
-   class Geometry_Rectangle
-    {
-		public  $width = 60;
-	    public $length = 100;
-
-        function rectange ()
-        {
-	        $area = $this->width * $this->length;
-	        return $area;
-        }
+	/**
+	 * Конструктор объекта окружности принимающий радиус
+	 * @param $radius
+	 */
+	function Circle($radius)
+	{
+		$this->radius = $radius;
 	}
 
-    $new_rectangle = new Geometry_Rectangle;
-    $new_rectangle ->rectange ();
-	echo $new_rectangle ->rectange ();
+	/**
+	 * Возвращает площать окружности
+	 * @return float
+	 */
+	function area()
+	{
+		$area = M_PI * $this->radius * $this->radius;
+		return $area;
+	}
+}
 
+// Пример создания окружности и получения ее площади
+$circle1 = new Circle(10);
+$area = $circle1->area();
+echo "Площадь окружности: " . $area;
 
+/**
+ * Class Rectangle
+ * Класс описывающий прямоугольник
+ */
+class Rectangle
+{
+	/**
+	 * Ширина прямоугольника
+	 * @var
+	 */
+	var $width;
+	/**
+	 * Высота примоугольника
+	 * @var
+	 */
+	var $height;
 
-    class Geometry_Square extends Geometry_Rectangle
-    {
-	    public  $length = 60;
-        function square ()
-        {
-	        $ar = $this->width * $this->length;
-	        return $ar;
-        }
-    }
-	$new_square = new Geometry_Square();
-	$new_square->square();
-	echo $new_square->square();
+	/**
+	 * Конструктор прямоульника принимающий ширину и высоту
+	 * @param $width
+	 * @param $height
+	 */
+	function Rectangle($width, $height)
+	{
+		$this->width = $width;
+		$this->height = $height;
+	}
+
+	function area()
+	{
+		return $this->width * $this->height;
+	}
+}
+
+// Пример создания прямоугольника и получение его площади
+$new_rectangle = new Rectangle(10, 20);
+$area = $new_rectangle->area();
+echo "\nПлощадь прямоугольника: " . $area;
+
+/**
+ * Class Square
+ * Класс описывающий квадрат
+ */
+class Square extends Rectangle
+{
+	/**
+	 * Сторона квадрата
+	 * @var
+	 */
+	var $side;
+
+	/**
+	 * Констуркутор создающий квадрат по стороне
+	 * @param $side
+	 */
+	function Square($side)
+	{
+		$this->width = $side;
+		$this->height = $side;
+	}
+}
+
+// Пример использования
+$new_square = new Square(10);
+$area = $new_square->area();
+echo "\nПлощадь квадрата: " . $area;
