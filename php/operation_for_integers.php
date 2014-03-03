533579eea096a6cfbf3c1388fc59f9ea9e6c03a2
mkursin <kursik88@gmail.com>
2014-03-03 11:08:17 +0400

    Небольшая правка оформления кода статического класса

--- php/operation_for_integers.php
+++ php/operation_for_integers.php
@@ -1,35 +1,25 @@
 <?php
+
 /**
- * User: Michail
- * Created: 25.02.14, 16:08
+ * Class Integer
+ * Класс для операций с целыми числами
  */
-
-/*
- * Создать статический класс для работы с целыми числами. В класс можно добавить любые методы для каких-либо вычислений.
- */
-    class Integers
-    {
-
-        public static function addition ($a, $b, $c)
-        {
-            if ($d = $a + $b + $c);
-            echo "$d";
-			return $d;
-        }
-
-        public static  function  subtraction ($a, $b, $c)
-        {
-            if ($d = $a - $b +$c);
-            echo "$d";
-			return $d;
-        }
-
-        public static  function  arr ()
-        {
-        $result = array($a, $b, $c);
-        return (int)$result;
-        }
+class Integer
+{
+	/**
+	 * Сложение 3х чисел
+	 * @param $a
+	 * @param $b
+	 * @param $c
+	 * @return mixed
+	 */
+	static function addition($a, $b, $c)
+	{
+		return $a + $b + $c;
 	}
 
-print_r ( Integers::subtraction(1,2,3));
-print_r ( Integers::subtraction(1,3,4));
+	// ...
+	// прочие методы которые могут быть :)
+}
+
+print_r(Integer::addition(1, 323, 12));
