e32bc6f74f35abcdf94f3378b6c810c0297e2249
mkursin <kursik88@gmail.com>
2014-03-03 10:14:40 +0400

    Комментарии для кода, рефакторинг

--- php/yandex_translate.php
+++ php/yandex_translate.php
@@ -1,10 +1,20 @@
 <?php
 
+/*
+ * Скрипт для перевода слов через Яндекс.Переводчик
+ */
+
+// Для Composer
 require 'vendor/autoload.php';
 
+// Константы для Яндекс.Переводчика
 define("YA_TRANSLATE_URL", "https://translate.yandex.net/api/v1.5");
 define("YA_TRANSLATE_KEY", "trnsl.1.1.20140227T084741Z.d3de9e0d146bce27.4fec6ec140e62e9b799eaf1c28b30e735fac59dd&ui=ru");
 
+/**
+ * Получает направления перевода
+ * @return array направления перевода
+ */
 function get_translate_directions()
 {
 	$url = YA_TRANSLATE_URL . "/tr.json/getLangs?key=" . YA_TRANSLATE_KEY . "&ui=ru";
@@ -12,9 +22,14 @@ function get_translate_directions()
 	$response = Requests::get($url, $headers);
 	$langs = json_decode($response->body);
 	return $langs->dirs;
-
 }
 
+/**
+ * Перевод слова на нужный язык
+ * @param  string $str строка на исходном языке
+ * @param  string $lng язык перевода
+ * @return string переведенную строку
+ */
 function translate($str, $lng)
 {
 	$url = YA_TRANSLATE_URL . "/tr.json/translate?key=" . YA_TRANSLATE_KEY . "&ui=ru&text=" . $str . "&lang=" . $lng;
@@ -23,27 +38,14 @@ function translate($str, $lng)
 	$body = json_decode($response->body);
 	return $body->text[0];
 }
-	/*
-	 * Пока данный код не буду удалять, может пригодиться для доработки
-	 */
-	/*echo "Введите направление перевода (например:ru-en). Внимание! По умолчанию будет заданно направленеи перевода  ru-en:";
-$original = fgets(STDIN);
-$lng  = fgets(STDIN);
-$lng = "ru-en";
-if ($lng  = fgets(STDIN) == true)
-	{
-		$lng = "ru-en";
-	}
-*/
 
-echo "Русско-английский переводчик \n Введите  перводимую фразу  или предложение и нажмите Enter:";
-	if ($original  = fgets(STDIN))
-/*if ($original  = fgets(STDIN) == true)
-	{
-	}
-$original = 'Привет, как дела?';*/
+// Пример использования переводчика:
+
+echo "Перевод посредством Яндекс.Переводчик \n Введите  перводимую фразу  или предложение и нажмите Enter:";
+$original = fgets(STDIN);
 $lng = "ru-en";
 $result = translate($original, $lng);
+
 echo "Переводимая строка: $original \n";
 echo "Переведенная строка: ";
 echo $result;
