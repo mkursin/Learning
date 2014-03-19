d76a62d1da7996f0aff20249f5adcb8d62a0f42d
mkursin <kursik88@gmail.com>
2014-03-19 16:11:52 +0400

    Доработан код, убрано лишнее, количество отображаемых страниц можно задавать в index.html

--- comments_page/get_comments.php
+++ comments_page/get_comments.php
@@ -25,18 +25,17 @@ $res = mysql_query($q);
 $row = mysql_fetch_row($res);
 $total_rows = $row[0]; // количество записей в базе
 
-$limit = 10; // количество записей на странице
+$limit = $_GET['limit']; // количество записей на странице
 $num_page = $_GET['page']; // номер страницы
 $count_pages = ceil($total_rows / $limit); // количество страниц
 
-// Если номер страницы оказался больше количества страниц, выведется последняя страница
+// Если номер страницы оказался больше количества страниц, выведется первая страница
 if ($num_page > $count_pages) $num_page = 1;
 
 $start_position = ($num_page - 1) * $limit; // позиция начала выборки данных
 
 $sql = "SELECT * FROM book LIMIT $start_position, $limit;" or die ("Ошибка запроса: ");
 $results = mysql_query($sql);
-//mysql_num_rows($results);
 
 if (!$results) {
     echo "Could not successfully run query ($sql) from DB: " . mysql_error();
@@ -56,8 +55,10 @@ else {
 
     $jsn = array(
         "count_pages" => $count_pages,
-        "page" => $num_page,
-        "items" => $rows
+        "page"        => $num_page,
+        "limit"       => $limit,
+        "total_items" => $total_rows,
+        "items"       => $rows
     );
 
     echo json_encode($jsn);
